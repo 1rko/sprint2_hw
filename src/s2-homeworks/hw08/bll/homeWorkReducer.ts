a2607dfe65bf5a3c39c32a4eb98ebbf3f7abe3f8
1rko <irkoabc@gmail.com>
2025-06-11 14:57:06 +0300

    Sprint3-HW8 Table, sort with reducer

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -4,15 +4,23 @@ type ActionType =
     | { type: 'sort'; payload: 'up' | 'down' }
     | { type: 'check'; payload: number }
 
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
+export const homeWorkReducer = (state: UserType[], action: ActionType): UserType[] => { // need to fix any
     switch (action.type) {
         case 'sort': { // by name
-
-            return state // need to fix
+            if (action.payload === 'up') {
+                let stateCopy=[...state]
+                return [...stateCopy.sort((a, b) =>
+                    (a.name > b.name) ? 1 : -1
+                )]
+            } else {
+                let stateCopy=[...state]
+                return [...stateCopy.sort((a, b) =>
+                    (a.name <= b.name) ? 1 : -1
+                )]
+            }
         }
         case 'check': {
-
-            return state // need to fix
+            return [...state.filter(user => user.age >= action.payload)]
         }
         default:
             return state
